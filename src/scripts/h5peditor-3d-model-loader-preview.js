f1bef5a17a57dfa9b185a73285bf586807928818
jballmann <johannes.ballmann@rwth-aachen.de>
2021-05-31 15:12:45 +0200

    init

---
+++ src/scripts/h5peditor-3d-model-loader-preview.js
@@ -0,0 +1,318 @@
+class ThreeDModelLoaderPreview {
+  /**
+   * 3D Preview of model
+   * @class H5PEditor.ThreeDModelLoaderPreview
+   */
+  constructor(params = {}, callbacks = {}) {
+    this.params = params || {};
+    this.params.plane = this.params.plane || false;
+
+    this.callbacks = callbacks || {};
+    this.callbacks.onIframeComplete = callbacks.onIframeComplete || (() => {});
+
+    this.iframe = this.buildIframe();
+  }
+
+  /**
+   * Get scene DOM.
+   * @return {HTMLElement} Scene DOM.
+   */
+  getDOM() {
+    return this.iframe;
+  }
+
+  /**
+   * Build iframe.
+   * @param {HTMLElement} iframe.
+   */
+  buildIframe() {
+    const iframe = document.createElement('iframe');
+    iframe.classList.add('h5p-editor-3d-model-loader-preview-iframe');
+
+    iframe.addEventListener('load', () => {
+      if (this.iframeLoaded) {
+        return;
+      }
+
+      // Will write the iframe contents
+      this.handleIframeLoaded(this.iframe);
+      this.iframeLoaded = true;
+    });
+
+    return iframe;
+  }
+
+  /**
+   * Handle iframe loaded.
+   * @param {HTMLElement} iframe Iframe.
+   */
+  handleIframeLoaded(iframe) {
+    try {
+      const iframeWindow = iframe.contentWindow;
+
+      // Write iframe contents
+      iframe.contentWindow.document.open();
+      iframe.contentWindow.document.write(this.buildHTML().outerHTML);
+      iframe.contentWindow.document.close();
+
+      this.iframeDocument = iframe.contentDocument ? iframe.contentDocument: iframeWindow;
+
+      this.handleIframeComplete();
+    }
+    catch (error) {
+      console.warn(error);
+    }
+  }
+
+  /**
+   * Build HTML for iframe.
+   * @return {HTMLElement}
+   */
+  buildHTML() {
+    const html = document.createElement('html');
+    html.appendChild(this.buildHeader());
+    html.appendChild(this.buildBody());
+
+    return html;
+  }
+
+  /**
+   * Build Header.
+   * @return {HTMLElement} Header.
+   */
+  buildHeader() {
+    const head = document.createElement('head');
+
+    // There must be a way to build the style dynamically from H5P libraries
+
+    // Load AFrame script
+    const scriptAFrame = document.createElement('script');
+    scriptAFrame.text = H5P.AFrame.toString();
+    head.appendChild(scriptAFrame);
+
+    const scriptAFrameOrbitControls = document.createElement('script');
+    scriptAFrameOrbitControls.text = H5P.AFrameOrbitControls.toString();
+    head.appendChild(scriptAFrameOrbitControls);
+
+    // Start scripts
+    const scriptStarter = document.createElement('script');
+    scriptStarter.text  = 'H5PAFrame();';
+    scriptStarter.text += 'H5PAFrameOrbitControls();';
+    head.appendChild(scriptStarter);
+
+    return head;
+  }
+
+  /**
+   * Build body.
+   * @return {HTMLElement} Body.
+   */
+  buildBody() {
+    const body = document.createElement('body');
+    body.style.background = 'url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAIAAAD8GO2jAAABhGlDQ1BJQ0MgcHJvZmlsZQAAKJF9kT1Iw0AcxV9TtVIqDnYQcQhSnSxIFXHUKhShQqgVWnUwufQLmjQkKS6OgmvBwY/FqoOLs64OroIg+AHi5Oik6CIl/i8ptIj14Lgf7+497t4BQr3MNKtrAtB020wl4mImuyoGXtGDEIKIYURmljEnSUl0HF/38PH1LsqzOp/7c/SpOYsBPpF4lhmmTbxBPL1pG5z3icOsKKvE58TjJl2Q+JHrisdvnAsuCzwzbKZT88RhYrHQxkobs6KpEU8RR1RNp3wh47HKeYuzVq6y5j35C0M5fWWZ6zSHkcAiliBBhIIqSijDRpRWnRQLKdqPd/APuX6JXAq5SmDkWEAFGmTXD/4Hv7u18pMxLykUB7pfHOdjFAjsAo2a43wfO07jBPA/A1d6y1+pAzOfpNdaWuQI6N8GLq5bmrIHXO4Ag0+GbMqu5Kcp5PPA+xl9UxYYuAWCa15vzX2cPgBp6ip5AxwcAmMFyl7v8O7e9t7+PdPs7weB03KtCTN/SAAAAAlwSFlzAAAuIwAALiMBeKU/dgAAAAd0SU1FB+QGDAsAMkAVB00AAAAZdEVYdENvbW1lbnQAQ3JlYXRlZCB3aXRoIEdJTVBXgQ4XAAAAPklEQVRIx2M8ceIEAynA1NSUJPVMDDQGoxaMWjBqwagFoxZQAzD+/fuXJA2k1h+jcTBqwagFoxaMWjAkLAAAEQ8IhUX6aEEAAAAASUVORK5CYII=")';
+    body.style.margin = '0';
+    body.style.overflow = 'hidden';
+    body.style.padding = '0';
+    body.style.height = '144px';
+    body.style.width = '256px';
+
+    body.appendChild(this.buildScene());
+
+    return body;
+  }
+
+  /**
+   * Build Scene.
+   * @return {HTMLElement} Scene.
+   */
+  buildScene() {
+    // Scene
+    const scene = document.createElement('a-scene');
+    scene.setAttribute('embedded', '');
+    scene.setAttribute('vr-mode-ui', 'false');
+
+    // Model
+    const entityModel = document.createElement('a-entity');
+    const model = document.createElement('a-gltf-model');
+    model.setAttribute('id', 'model');
+    entityModel.appendChild(model);
+    scene.appendChild(entityModel);
+
+    if (this.params.plane) {
+      // Plane
+      const markerPlane = document.createElement('a-plane');
+      markerPlane.setAttribute('id', 'markerPlane');
+      markerPlane.setAttribute('position', `${ThreeDModelLoaderPreview.DEFAULT_OFFSET.x} ${ThreeDModelLoaderPreview.DEFAULT_OFFSET.y} ${ThreeDModelLoaderPreview.DEFAULT_OFFSET.z}`);
+      markerPlane.setAttribute('rotation', '-90 0 0');
+      markerPlane.setAttribute('width', '1');
+      markerPlane.setAttribute('height', '1');
+      markerPlane.setAttribute('src', ThreeDModelLoaderPreview.DEFAULT_TEXTURE);
+      markerPlane.setAttribute('shadow', '');
+
+      scene.appendChild(markerPlane);
+    }
+
+    // Camera
+    const entityCamera = document.createElement('a-entity');
+    entityCamera.setAttribute('camera', '');
+    entityCamera.setAttribute('look-controls', '');
+    entityCamera.setAttribute('orbit-controls', 'initialPosition: 0 0 2; enableKeys: false;');
+    scene.appendChild(entityCamera);
+
+    return scene;
+  }
+
+  /**
+   * Handle iframe complete. Depends on load timing.
+   */
+  handleIframeComplete() {
+    if (this.iframeDocument.readyState !== 'complete') {
+      this.iframeDocument.addEventListener('readystatechange', () => {
+        if (this.iframeDocument.readyState === 'complete') {
+
+          this.model = this.iframeDocument.querySelector('#model');
+          this.markerPlane = this.iframeDocument.querySelector('#markerPlane');
+
+          this.isInitialized = true;
+          this.callbacks.iframeComplete();
+        }
+      });
+    }
+    else {
+      this.model = this.iframeDocument.querySelector('#model');
+      this.markerPlane = this.iframeDocument.querySelector('#markerPlane');
+
+      this.isInitialized = true;
+      this.callbacks.onIframeComplete();
+    }
+  }
+
+  /**
+   * Show preview.
+   */
+  show() {
+    this.iframe.classList.remove('h5peditor-3d-model-loader-display-none');
+  }
+
+  /**
+   * Hide preview.
+   */
+  hide() {
+    this.iframe.classList.add('h5peditor-3d-model-loader-display-none');
+  }
+
+  /**
+   * Set marker texture. If no src parameter, remove texure.
+   * @param {string} [src] Base64 encoded image source path.
+   */
+  setMarkerTexture(src = ThreeDModelLoaderPreview.DEFAULT_TEXTURE) {
+    if (!this.isInitialized) {
+      return; // Not ready
+    }
+
+    if (!this.markerPlane) {
+      return; // No marker plane
+    }
+
+    this.markerPlane.setAttribute('src', src);
+  }
+
+  /**
+   * Set model.
+   * @param {string} path Object file path.
+   * @param {object} params Parameters.
+   */
+  setModel(path, params) {
+    if (!this.isInitialized) {
+      return;
+    }
+
+    if (!path) {
+      this.model.setAttribute('visible', false);
+      return;
+    }
+
+    // Get potential cross-origin source
+    const element = document.createElement('div');
+    H5P.setSource(element, {path:path}, H5PEditor.contentId);
+    const src = element.src;
+
+    if (!src) {
+      return;
+    }
+
+    // Set model
+    this.model.setAttribute('gltf-model', src);
+
+    // Set geometry
+    if (params && params.scale) {
+      this.setModelScale(params.scale);
+    }
+
+    if (params && params.position) {
+      this.setModelPosition(params.position);
+    }
+
+    if (params && params.rotation) {
+      this.setModelRotation(params.rotation);
+    }
+
+    this.model.setAttribute('visible', true);
+  }
+
+  /**
+   * Set model scale.
+   * @param {number} scale Scale relative to original model scale.
+   */
+  setModelScale(scale) {
+    if (!this.isInitialized || !scale) {
+      return;
+    }
+
+    scale = Math.max(0.01, scale);
+
+    this.model.setAttribute('scale', `${scale} ${scale} ${scale}`);
+  }
+
+  /**
+   * Set model position.
+   * @param {object} coordinates Coordinates relative to marker.
+   * @param {number} coordinates.x X coordinate.
+   * @param {number} coordinates.y Y coordinate.
+   * @param {number} coordinates.z Z coordinate.
+   */
+  setModelPosition(coordinates) {
+    if (!this.isInitialized || !coordinates) {
+      return;
+    }
+
+    const x = coordinates.x + ThreeDModelLoaderPreview.DEFAULT_OFFSET.x;
+    const y = coordinates.y + ThreeDModelLoaderPreview.DEFAULT_OFFSET.y;
+    const z = coordinates.z + ThreeDModelLoaderPreview.DEFAULT_OFFSET.z;
+
+    this.model.setAttribute('position', `${x} ${y} ${z}`);
+  }
+
+  /**
+   * Set model rotation.
+   * @param {object} angles Angles relative to marker.
+   * @param {number} angles.x X angle.
+   * @param {number} angles.y Y angle.
+   * @param {number} angles.z Z angle.
+   */
+  setModelRotation(angles) {
+    if (!this.isInitialized || !angles) {
+      return;
+    }
+
+    this.model.setAttribute('rotation', `${angles.x} ${angles.y} ${angles.z}`);
+  }
+}
+
+// Default offset
+ThreeDModelLoaderPreview.DEFAULT_OFFSET = {x: 0, y: -1, z: 0};
+
+// White texture
+ThreeDModelLoaderPreview.DEFAULT_TEXTURE = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAABhWlDQ1BJQ0MgcHJvZmlsZQAAKJF9kT1Iw0AcxV9btSoVBzOIOGSogmBBVMRRq1CECqFWaNXB5NIPoUlDkuLiKLgWHPxYrDq4OOvq4CoIgh8gTo5Oii5S4v+SQosYD4778e7e4+4dEKyVmGa1jQGabpupRFzMZFfE8Cs6EIGAEXTJzDJmJSkJ3/F1jwBf72I8y//cn6NHzVkMCIjEM8wwbeJ14qlN2+C8TyywoqwSnxOPmnRB4keuKx6/cS64HOSZgplOzRELxGKhhZUWZkVTI54kjqqaTvnBjMcq5y3OWqnCGvfkL4zk9OUlrtMcRAILWIQEEQoq2EAJNmK06qRYSNF+3Mc/4Polcink2gAjxzzK0CC7fvA/+N2tlZ8Y95IicaD9xXE+hoDwLlCvOs73sePUT4DQM3ClN/3lGjD9SXq1qUWPgN5t4OK6qSl7wOUO0P9kyKbsSiGawXweeD+jb8oCfbdA96rXW2Mfpw9AmrpK3gAHh8BwgbLXfN7d2drbv2ca/f0ARjFylR0Z2fIAAAAJcEhZcwAALiMAAC4jAXilP3YAAAAHdElNRQfkBgwUKTdjQ4hkAAAAGXRFWHRDb21tZW50AENyZWF0ZWQgd2l0aCBHSU1QV4EOFwAAAAxJREFUCNdj+P//PwAF/gL+3MxZ5wAAAABJRU5ErkJggg==';
+
+export default ThreeDModelLoaderPreview;
---
+++ src/scripts/h5peditor-3d-model-loader.js
@@ -0,0 +1,482 @@
+import ThreeDModelLoaderConversionDropzone from './h5peditor-3d-model-loader-conversion-dropzone.js';
+import ThreeDModelLoaderPreview from './h5peditor-3d-model-loader-preview.js';
+
+class ThreeDModelLoader {
+  /**
+   * Used to load additional GLTF resources
+   * @class H5PEditor.ThreeDModelLoader
+   *
+   * @param {object} parent Parent object in semantics hierarchy.
+   * @param {object} field Fields in semantics where widget is used.
+   * @param {object} params Parameters of form.
+   * @param {function} setValue Callback to update the form value.
+   *
+   * @throws {Error} No image found.
+   */
+  constructor(parent, field, params, setValue) {
+    this.parent = parent;
+    this.field = field;
+    this.params = params;
+    this.setValue = setValue;
+
+    // Sanitize field parameters
+    this.field.threeDModelLoader = this.field.threeDModelLoader || {};
+    this.field.threeDModelLoader.fileTypeExtensions = this.field.threeDModelLoader.fileTypeExtensions || ['gltf', 'glb'];
+    this.field.threeDModelLoader.geometryPath = this.field.threeDModelLoader.geometryPath || '';
+    this.field.threeDModelLoader.planePatternPath = this.field.threeDModelLoader.planePatternPath || '';
+    this.field.threeDModelLoader.preview = this.field.threeDModelLoader.preview || false;
+
+    // IE11 can neither handle preview nor conversion
+    this.canPreview = this.field.threeDModelLoader.preview && window.navigator.userAgent.indexOf('Trident/') === -1;
+    this.canConvert = window.navigator.userAgent.indexOf('Trident/') === -1;
+
+    this.queue = [];
+
+    // Create the wrapper:
+    this.$container = H5P.jQuery('<div>', {
+      'class': 'field h5peditor-3d-model-loader-container'
+    });
+
+    const widgetName = this.field.type;
+    this.fieldInstance = new H5PEditor.widgets[widgetName](parent, field, params, setValue);
+    this.fieldInstance.appendTo(this.$container);
+
+    // Errors
+    this.$errors = this.$container.children().children('.h5p-errors');
+
+    if (this.canPreview) {
+      // Create preview
+      this.preview = new ThreeDModelLoaderPreview(
+        {
+          plane: this.field.threeDModelLoader.planePatternPath !== ''
+        },
+        {
+          onIframeComplete: (() => {
+            // Potentially init stuff only now
+          })
+        });
+      this.$container.get(0).appendChild(this.preview.getDOM());
+
+      // Update scene plane for marker
+      if (this.field.threeDModelLoader.planePatternPath) {
+        const arMarker = H5PEditor.findField(this.field.threeDModelLoader.planePatternPath, this.parent);
+        if (arMarker) {
+          arMarker.on('removedMarkerPattern', () => {
+            this.preview.setMarkerTexture();
+          });
+
+          arMarker.on('addedMarkerPattern', (event) => {
+            this.preview.setMarkerTexture(event.data);
+          });
+        }
+      }
+    }
+
+    this.parent.ready( () => {
+
+      if (this.canConvert) {
+        // Create dropzone
+        this.dropzone = this.dropzone || new ThreeDModelLoaderConversionDropzone((result) => {
+          this.handleConversionDone(result);
+        });
+        const container = this.$container.get(0);
+        container.parentNode.insertBefore(this.dropzone.getDOM(), container.nextSibling);
+      }
+
+      // It might be better to have a complete geometry editor widget
+      const geometryPath = this.field.threeDModelLoader.geometryPath;
+
+      if (this.field.threeDModelLoader.geometryPath !== '') {
+        // Listen for scale change
+        this.rowScale = H5PEditor.findField(`${geometryPath}/scale`, this.parent);
+        if (this.rowScale && this.canPreview) {
+          this.rowScale.on('changed', (event) => {
+            this.preview.setModelScale(
+              parseFloat(event.data.scale) / 100
+            );
+          });
+        }
+
+        // Listen for position change
+        this.rowPosition = H5PEditor.findField(`${geometryPath}/position`, this.parent);
+        if (this.rowPosition && this.canPreview) {
+          this.rowPosition.on('changed', (event) => {
+            this.preview.setModelPosition({
+              x: parseFloat(event.data.x),
+              y: parseFloat(event.data.y),
+              z: parseFloat(event.data.z)
+            });
+          });
+        }
+
+        // Listen for rotation change
+        this.rowRotation = H5PEditor.findField(`${geometryPath}/rotation`, this.parent);
+        if (this.rowRotation && this.canPreview) {
+          this.rowRotation.on('changed', (event) => {
+            this.preview.setModelRotation({
+              x: parseFloat(event.data.x),
+              y: parseFloat(event.data.y),
+              z: parseFloat(event.data.z)
+            });
+          });
+        }
+      }
+    });
+
+    // Changes
+    this.changes = [];
+
+    this.fileIcon = document.createElement('div');
+    this.fileIcon.classList.add('h5peditor-3d-model-loader-file-icon');
+
+    // Update icon on loadup
+    H5PEditor.followField(this.parent, this.field.name, (event) => {
+      if (!event || !event.path) {
+        this.resetModel();
+        return;
+      }
+
+      // Only gltf supported
+      const extension = event.path.split('.').pop().toLowerCase();
+      if (this.field.threeDModelLoader.fileTypeExtensions.indexOf(extension) === -1) {
+        return;
+      }
+
+      this.setModel(event.path);
+    });
+
+    this.fieldInstance.on('uploadProgress', () => {
+      if (this.canPreview) {
+        this.preview.hide();
+      }
+
+      this.resetGeometry();
+    });
+
+    // React on file changes
+    this.fieldInstance.changes.push((event) => {
+      if (!event) {
+        this.resetModel();
+      }
+      else {
+        this.handleFileUploaded(event.path);
+      }
+    });
+  }
+
+  /**
+   * Reset geometry.
+   */
+  resetGeometry() {
+    [this.rowScale, this.rowPosition, this.rowRotation].forEach(row => {
+      if (!row) {
+        return;
+      }
+
+      row.children.forEach(child => {
+        child.$input.val(child.field.default);
+        child.$input.change();
+      });
+    });
+  }
+
+  /**
+   * Reset model.
+   */
+  resetModel() {
+    if (this.canPreview) {
+      this.preview.setModel();
+      this.preview.hide();
+    }
+
+    this.resetGeometry();
+  }
+
+  /**
+   * Set model in preview.
+   * @param {string} path Full URL path to model file.
+   */
+  setModel(path) {
+    const extension = path.replace(/#tmp$/, '').split('.').pop().toLowerCase();
+
+    this.showFileIcon(extension);
+
+    if (this.canConvert) {
+      this.dropzone.hide();
+    }
+
+    if (this.canPreview) {
+      this.preview.setModel(path, this.getGeometry());
+      this.preview.show();
+    }
+  }
+
+  /**
+   * Get current geometry.
+   * @return {object} Geometry data.
+   */
+  getGeometry() {
+    // Yes, you'd make sure the children are in place and number fields...
+    return {
+      scale: parseFloat(this.rowScale.children[0].$input.val()) / 100,
+      position: {
+        x: parseFloat(this.rowPosition.children[0].$input.val()),
+        y: parseFloat(this.rowPosition.children[1].$input.val()),
+        z: parseFloat(this.rowPosition.children[2].$input.val()),
+      },
+      rotation: {
+        x: parseFloat(this.rowRotation.children[0].$input.val()),
+        y: parseFloat(this.rowRotation.children[1].$input.val()),
+        z: parseFloat(this.rowRotation.children[2].$input.val()),
+      }
+    };
+  }
+
+  /**
+   * Handle new file uploaded.
+   * @param {string} path Path to model.
+   */
+  handleFileUploaded(path) {
+    const extension = path.split('#').slice(0, -1).join('#').split('.').slice(-1)[0].toLowerCase();
+
+    // Only gltf supported
+    if (this.field.threeDModelLoader.fileTypeExtensions.indexOf(extension) === -1) {
+      this.$errors.append(H5PEditor.createError(H5PEditor.t('H5PEditor.ThreeDModelLoader', 'filetypeNotSupported')));
+      this.removeFile();
+
+      if (this.canConvert) {
+        this.dropzone.hide();
+      }
+
+      return;
+    }
+
+    /*
+     * GLTF files are JSON files, but may not be in embedded format with base64
+     * encoded textures or bin files. Would require to upload assets separately
+     * and encode them. For now, only allow GLTF in embedded format and GLB.
+     */
+    if (extension === 'gltf') {
+      // Get potential cross-origin source
+      const element = document.createElement('div');
+      H5P.setSource(element, {path: path}, H5PEditor.contentId);
+      const src = element.src;
+
+      // Try to parse JSON from file
+      try {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', src);
+        xhr.onreadystatechange = () => {
+          if (xhr.readyState !== 4) {
+            return;
+          }
+
+          try {
+            const json = JSON.parse(xhr.responseText);
+
+            // Three.JS cannot handle glTF v1.0.
+            if (!this.isGLTFVersionTwo(json)) {
+              this.handleError(H5PEditor.t('H5PEditor.ThreeDModelLoader', 'onlyVersionTwo'));
+              return;
+            }
+
+            // Can't upload multiple files or zip files, offer conversion
+            if (!this.isGLTFEmbeddedFormat(json)) {
+              if (this.canConvert) {
+                this.handleError(H5PEditor.t('H5PEditor.ThreeDModelLoader', 'onlyEmbeddedAssets'));
+                this.handleEmbeddedAssets();
+              }
+              else {
+                this.handleError(H5PEditor.t('H5PEditor.ThreeDModelLoader', 'browserCannotConvert'));
+              }
+
+              return;
+            }
+          }
+          catch (error) {
+            this.handleError(`${H5PEditor.t('H5PEditor.ThreeDModelLoader', 'fileDamaged')} (${error})`);
+            return;
+          }
+
+          this.setModel(path);
+        };
+        xhr.send();
+      }
+      catch (error) {
+        this.handleError(`${H5PEditor.t('H5PEditor.ThreeDModelLoader', 'fileDamaged')} (${error})`);
+        return;
+      }
+    }
+    else {
+      this.setModel(path);
+    }
+  }
+
+  /**
+   * Handle embedded assets uploaded.
+   */
+  handleEmbeddedAssets() {
+    // Add dropzone to DOM
+    if (this.canConvert) {
+      this.dropzone.show();
+    }
+  }
+
+  /**
+   * Handle conversion done.
+   * @param {object} result Result.
+   * @param {object} [result.file] File data.
+   * @param {string} [result.error] Error message.
+   */
+  handleConversionDone(result) {
+    if (result.error) {
+      this.handleError(`${H5PEditor.t('H5PEditor.ThreeDModelLoader', 'conversionError')} (${result.error})`);
+      return;
+    }
+
+    // Hope all's sanitized here with the file
+    this.fieldInstance.upload(result.file, 'foo.glb');
+
+    if (this.canConvert) {
+      this.dropzone.hide();
+    }
+  }
+
+  /**
+   * Check JSON of GLTF file for being embedded format.
+   * @param {object} json GLTF file as JSON object.
+   * @return {boolean} True, if is embedded format, else false.
+   */
+  isGLTFEmbeddedFormat(json) {
+    const objects = this.findObjects(json, 'uri', '');
+    const containsExternalReference = objects.some((obj) => {
+      return obj.uri.substr(0, 5) !== 'data:';
+    });
+
+    return !containsExternalReference;
+  }
+
+  /**
+   * Check JSON of GLTF file for version strings.
+   * @param {object} json GLTF file as JSON object.
+   * @return {boolean} True, if no version is of 1.0.
+   */
+  isGLTFVersionTwo(json) {
+    const objects = this.findObjects(json, 'version', '');
+
+    const containsOnePointO = objects.some((obj) => {
+      return obj.version === '1.0';
+    });
+
+    return !containsOnePointO;
+  }
+
+  /**
+   * Get objects with particular key (and value) from object.
+   * @param {object} obj Object to search in.
+   * @param {string} key Key to search for.
+   * @param {string} val Value to search for.
+   */
+  findObjects(obj, key, val) {
+    let objects = [];
+    for (var i in obj) {
+      if (!obj.hasOwnProperty(i)) {
+        continue;
+      }
+
+      if (typeof obj[i] == 'object') {
+        objects = objects.concat(this.findObjects(obj[i], key, val));
+      }
+      else {
+        if (i === key && obj[i] === val || i === key && val === '') {
+          objects.push(obj);
+        }
+        else if (obj[i] === val && key === '') {
+          if (objects.lastIndexOf(obj) === -1) {
+            objects.push(obj);
+          }
+        }
+      }
+    }
+
+    return objects;
+  }
+
+  /**
+   * Handle error.
+   * @param {string} errorMessage Error message text.
+   */
+  handleError(errorMessage) {
+    this.$errors.empty();
+    this.$errors.append(H5PEditor.createError(errorMessage));
+    this.removeFile();
+  }
+
+  /**
+   * Remove file from widget.
+   */
+  removeFile() {
+    delete this.fieldInstance.params;
+    this.fieldInstance.setValue(this.fieldInstance.field);
+    this.fieldInstance.addFile();
+
+    for (var i = 0; i < this.fieldInstance.changes.length; i++) {
+      this.fieldInstance.changes[i]();
+    }
+  }
+
+  /**
+   * Show file pseudo icon.
+   * @param {string} type File type.
+   */
+  showFileIcon(type) {
+    // Wait for image. File.addFile() might not have completed yet
+    const waitForImg = (timeout) => {
+      timeout = timeout || 500;
+
+      if (timeout <= 0) {
+        return;
+      }
+
+      const img = this.fieldInstance.$file.find('img').get(0);
+      if (img) {
+        this.fileIcon.title = type;
+        this.fileIcon.innerText = type;
+        img.style.display = 'none';
+        this.fieldInstance.$file.find('.thumbnail').get(0).appendChild(this.fileIcon);
+      }
+      else {
+        setTimeout(() => {
+          waitForImg(timeout - 20);
+        }, 20);
+      }
+    };
+
+    waitForImg();
+  }
+
+  /**
+   * Append field to wrapper. Invoked by H5P core.
+   * @param {H5P.jQuery} $wrapper Wrapper.
+   */
+  appendTo($wrapper) {
+    this.$container.appendTo($wrapper);
+  }
+
+  /**
+   * Validate current values. Invoked by H5P core.
+   * @return {boolean} True, if current value is valid, else false.
+   */
+  validate() {
+    return this.fieldInstance.validate();
+  }
+
+  /**
+   * Remove self. Invoked by H5P core.
+   */
+  remove() {
+    this.$container.remove();
+  }
+}
+
+export default ThreeDModelLoader;
